859db724f3bae0b3283fcf1b96a89ea01a1e049b
Yan Lima <yanlqueiroz@gmail.com>
2024-11-21 17:15:40 -0300

    adicionado os métodos "update" e "delete"

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ class Reserva(BaseModel):
         data_fim: str
         nivel_quarto: str # Standard, Master, Deluxe
 
-
 ##apenas cod teste
 @app.post('/reserva')
 def criar_reserva(reserva: Reserva):
@@ -23,8 +22,8 @@ def criar_reserva(reserva: Reserva):
         "message": "Reserva criada",
         "id_reserva": id_reserva,
         "dados": reserva
-        
     }
+
 reservas = {
     1: Reserva(id_quarto=101, id_cliente=202, data_inicio="2024-12-01", data_fim="2024-12-10", nivel_quarto="Deluxe"),
     2: Reserva(id_quarto=102, id_cliente=203, data_inicio="2024-12-05", data_fim="2024-12-12", nivel_quarto="Master")
