29054c2ba26951ce948041bec2016118b2b52ebc
Tatiely Nobre <73186753+tatielynobre@users.noreply.github.com>
2024-11-22 20:07:20 -0300

    Update converter_to_zip.py

--- repositories/converter_to_zip.py
+++ repositories/converter_to_zip.py
@@ -3,6 +3,6 @@ import zipfile ##precisa especificar qual o arquivo csv que ele vai compactar
 
 def Compact_csv(csv_file):
     zip_file = f"{csv_file}.zip"
-    with zipfile.ZipFile('csv_file', 'w') as zip:
+    with zipfile.ZipFile(csv_file, 'w') as zip:
             zip.write(csv_file)
     return zip_file
